6a5fe4395de02164767476cbab88a39c04df9ca1
mylostandromeda <49440391+mylostandromeda@users.noreply.github.com>
2019-04-15 00:54:31 +0300

    Update script.js

--- Happy_Birthday/script.js
+++ Happy_Birthday/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(".btn").click(function(){
         $(".default").toggleClass("active");
         $(".head").toggleClass("head-close");
+        $(".iframe").toggleClass("iframe-active");
         $(".main-block").show(500);
         return false;
     });
