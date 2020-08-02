9789488f0540b4935c8f86d0db0187cb415d9aca
Artur Verbin <my.lost.andromeda@gmail.com>
2020-08-02 18:20:51 +0300

    Delete projects

--- Happy_Birthday/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".btn").click(function(){
-        $(".default").toggleClass("active");
-        $(".iframe").toggleClass("iframe-active");
-        $(".head").toggleClass("head-close");
-        $(".main-block").show(500);
-        return false;
-    });
-});
